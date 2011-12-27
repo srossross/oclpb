2036ffcc9538a7169c0ad1f0295bf30dddfcfd61
srossross <srossross@enthought.com>
2011-12-27 15:54:47 -0800

    Adding python 3 support.

--- opencl/__init__.py
+++ opencl/__init__.py
@@ -1,5 +1,8 @@
 
-from .copencl import Platform, get_platforms, Device, Program, UserEvent, Event
+from .copencl import Platform, get_platforms, Device
+from .program import Program
+from .event import UserEvent, Event
+
 from .context import Context, ContextProperties
 from .kernel import contextual_memory, global_memory, local_memory, constant_memory, Kernel
 from .queue import Queue
