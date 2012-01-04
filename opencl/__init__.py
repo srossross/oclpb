9fa2a7482a1d443d3123fd4dcd2ff3e04457ec5f
srossross <srossross@enthought.com>
2012-01-04 13:09:59 -0800

    Added case in setup.py where OpenGL is not installed.

--- opencl/__init__.py
+++ opencl/__init__.py
@@ -11,7 +11,12 @@ from .cl_mem import DeviceMemoryView, empty
 from .cl_mem import mem_layout, broadcast
 from .cl_mem import empty_image, Image, ImageFormat
 from .errors import OpenCLException
-import opencl.clgl as gl
+
+try:
+    import opencl.clgl as gl
+except:
+    gl = None
+
 
 from_host = DeviceMemoryView.from_host
 
